398569eda31e66fb0dee1d2d95b773fc50e00f20
John King <Johnk858@ymail.com>
2020-10-13 22:24:12 +0200

    Microsoft Entity Framework Core
    Packages needed to work with database

--- Project 2_26047179/Project 2_26047179/Startup.cs
+++ Project 2_26047179/Project 2_26047179/Startup.cs
@@ -23,7 +23,7 @@ namespace Project_2_26047179
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();//allows me to just refresh for design
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
