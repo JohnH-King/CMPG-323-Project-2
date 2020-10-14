52199e5641365580a9fb8d73a770471fbd5ef968
John King <Johnk858@ymail.com>
2020-10-15 00:44:14 +0200

    Starting hardcoded DB
    will try to make db create from Col 1

--- Project 2_26047179/Project 2_26047179/Model/ApplicationDbContext.cs
+++ Project 2_26047179/Project 2_26047179/Model/ApplicationDbContext.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Project_2_26047179.Model
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employee { get; set; }           
     }
 }
